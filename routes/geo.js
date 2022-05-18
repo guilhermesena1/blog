fe9fa5cf2ab81f65c70bc509474e8aa51d7a6f30
guilhermesena1 <desenabr@usc.edu>
2022-05-17 21:18:54 -0700

    adding more functionality to the WGBS database search

--- routes/geo.js
+++ routes/geo.js
@@ -19,17 +19,12 @@ router.get('/', async function(req, res, next) {
   // fetch info on most recent ids
   var fetch;
   const the_query_url = process.env.EFETCH_URL  + '&id=' + all_ids;
-  console.log(the_query_url);
   await axios.get(the_query_url).then((response) => {
     xml2js.parseString(response.data, (err, result) => {
       fetch = result.EXPERIMENT_PACKAGE_SET.EXPERIMENT_PACKAGE;
     })
   });
 
-  console.log(search);
-  console.log(fetch[0].Pool[0]);
-  console.log('search size: ' + search.IdList[0].Id.length);
-  console.log('fetch size: ' + fetch.length);
   data = {title : 'GEO WGBS datasets', search : search, fetch : fetch};
   res.render('geo', data);
 });
@@ -38,14 +33,12 @@ router.get('/:sraId', async function(req, res, next) {
   var ret;
   var the_id = req.params.sraId;
 
-  console.log('trying to access ID ' + the_id);
   await axios.get(process.env.EFETCH_URL  + '&id=' + the_id).then((response) => {
     xml2js.parseString(response.data, (err, result) => {
       ret = result.EXPERIMENT_PACKAGE_SET.EXPERIMENT_PACKAGE[0];
     })
   });
 
-  console.log(ret.EXPERIMENT[0].DESIGN[0]);
   data = {title : 'GEO query ' + the_id, id : the_id, ret : ret};
   res.render('geodataset', data);
 });
