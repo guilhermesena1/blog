264beb73be9847163bd98d92e238ef1675c20943
guilhermesena1 <desenabr@usc.edu>
2022-05-18 20:09:59 -0700

    creating pagination for the entries

--- routes/geo.js
+++ routes/geo.js
@@ -3,8 +3,12 @@ var axios = require('axios');
 var xml2js = require('xml2js');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+  res.redirect('/geo/0');
+});
+
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
+router.get('/:page', async function(req, res, next) {
   // fetch most recent datasets
   var search;
   await axios.get(process.env.ESEARCH_URL).then((response) => {
@@ -13,19 +17,41 @@ router.get('/', async function(req, res, next) {
     })
   });
 
-  // join all IDs by comma
-  const all_ids = search.IdList[0].Id.join(',');
-
   // fetch info on most recent ids
-  var fetch;
-  const the_query_url = process.env.EFETCH_URL  + '&id=' + all_ids;
+
+  var num_elems_per_page = 20;
+  var the_page = Number(req.params.page);
+
+  var the_ids = search.IdList[0].Id;
+  var ids_start = Math.min(the_page*num_elems_per_page, the_ids.length - 1);
+  var ids_end = Math.min((the_page + 1)*num_elems_per_page, the_ids.length);
+
+  console.log('page: ' + the_page);
+  console.log('ids start: ' + ids_start);
+  console.log('ids end: ' + ids_end);
+
+  var fetch_ids = the_ids.slice(ids_start, ids_end).join(',');
+  var fetch = [];
+
+  const the_query_url = process.env.EFETCH_URL  + '&id=' + fetch_ids;
   await axios.get(the_query_url).then((response) => {
     xml2js.parseString(response.data, (err, result) => {
       fetch = result.EXPERIMENT_PACKAGE_SET.EXPERIMENT_PACKAGE;
     })
   });
 
-  data = {title : 'GEO WGBS datasets', search : search, fetch : fetch};
+
+  num_pages = Math.ceil(the_ids.length/num_elems_per_page);
+  console.log(the_ids.length);
+  console.log(num_elems_per_page);
+  console.log(num_pages);
+  data = {
+           title : 'GEO WGBS datasets',
+           search : search,
+           fetch : fetch,
+           the_page : the_page,
+           num_pages : num_pages
+         };
   res.render('geo', data);
 });
 
