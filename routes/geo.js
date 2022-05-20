1b1ffc95ecac54cb32961d3b1a8815867b1c0178
guilhermesena1 <desenabr@usc.edu>
2022-05-19 20:18:02 -0700

    adding the functionality to get all SRR URLs for a project

--- routes/geo.js
+++ routes/geo.js
@@ -23,12 +23,16 @@ router.get('/:page', async function(req, res, next) {
   var the_page = Number(req.params.page);
 
   var the_ids = search.IdList[0].Id;
+  var tot_ids = the_ids.length; // total WGBS hits
+
+  // IDs that will be displayed in the page
   var ids_start = Math.min(the_page*num_elems_per_page, the_ids.length - 1);
   var ids_end = Math.min((the_page + 1)*num_elems_per_page, the_ids.length);
+  the_ids = the_ids.slice(ids_start, ids_end);
 
-  var fetch_ids = the_ids.slice(ids_start, ids_end).join(',');
+  // uses EFetch to get info on the IDs
+  var fetch_ids = the_ids.join(',');
   var fetch = [];
-
   const the_query_url = process.env.EFETCH_URL  + '&id=' + fetch_ids;
   await axios.get(the_query_url).then((response) => {
     xml2js.parseString(response.data, (err, result) => {
@@ -36,11 +40,11 @@ router.get('/:page', async function(req, res, next) {
     })
   });
 
-
-  num_pages = Math.ceil(the_ids.length/num_elems_per_page);
+  num_pages = Math.ceil(tot_ids/num_elems_per_page);
   data = {
            title : 'GEO WGBS datasets',
-           search : search,
+           total_hits : tot_ids,
+           the_ids : the_ids,
            fetch : fetch,
            the_page : the_page,
            num_pages : num_pages
@@ -48,17 +52,88 @@ router.get('/:page', async function(req, res, next) {
   res.render('geo', data);
 });
 
-router.get('/id/:sraId', async function(req, res, next) {
-  var ret;
-  var the_id = req.params.sraId;
+function  good_url(url) {
+  return(
+    !url.includes("fq") &&
+    !url.includes("fastq") &&
+    !url.includes("bam") &&
+    !url.includes("cram") &&
+     url.includes("https") &&
+     url.includes("sra"));
+}
 
+router.get('/id/:sraId', async function(req, res, next) {
+  var the_id = req.params.sraId;
+  console.log('fetching data for ' + the_id);
+
+  //get information from this SRX
+  var ret;
   await axios.get(process.env.EFETCH_URL  + '&id=' + the_id).then((response) => {
     xml2js.parseString(response.data, (err, result) => {
       ret = result.EXPERIMENT_PACKAGE_SET.EXPERIMENT_PACKAGE[0];
-    })
+    });
   });
 
-  data = {title : 'GEO query ' + the_id, id : the_id, ret : ret};
+  // get study ID
+  the_study = ret.EXPERIMENT[0].STUDY_REF[0].$.accession;
+  console.log('study: ' + the_study);
+
+  // Get IDs of same study
+  var same_study_ids;
+  search_url = process.env.ESEARCH_URL_STUDY + '(' + the_study + '[Study] AND Bisulfite-Seq[Strategy])';
+  await axios.get(search_url).then((response) => {
+    xml2js.parseString(response.data, (err, result) => {
+      same_study_ids = result.eSearchResult.IdList[0].Id;
+      console.log('hits: ' + same_study_ids.length);
+      same_study_ids = same_study_ids.join(',');
+    });
+  });
+
+  // get all SRR URLs from the same study
+  var srr_urls = []
+  const fetch_url = process.env.EFETCH_URL  + '&id=' + same_study_ids;
+  await axios.get(fetch_url).then((response) => {
+    xml2js.parseString(response.data, (err, result) => {
+      try {
+      result.EXPERIMENT_PACKAGE_SET
+            .EXPERIMENT_PACKAGE.forEach((experiment) => {
+        experiment.RUN_SET[0].RUN.forEach((the_run) => {
+          the_run.SRAFiles[0].SRAFile.forEach((the_file) => {
+            var found = false;
+            try {
+              const the_url = the_file.$.url;
+              if (typeof the_url !== "undefined") {
+                if (!found && good_url(the_url)) {
+                  srr_urls.push(the_url);
+                  found = true;
+                }
+              }
+
+              else if (typeof the_file.Alternatives !== "undefined") {
+                the_file.Alternatives.forEach((alternative) => {
+                  const the_alt_url = alternative.$.url;
+                  if (!found && good_url(the_url)) {
+                    srr_urls.push(the_alt_url);
+                    found = true;
+                  }
+                });
+              }
+            }
+            catch (e) {
+              console.log('Could not fetch data!');
+            }
+          });
+        });
+      });
+      }
+      catch (e) {
+        console.log('Failed getting data for experiment!');
+      }
+    });
+  });
+
+  console.log(srr_urls);
+  data = {title : 'GEO query ' + the_id, id : the_id, ret : ret, srr_urls : srr_urls};
   res.render('geodataset', data);
 });
 
