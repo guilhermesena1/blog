1ec2279cdc5957c373b73441ba6e918225886113
guilhermesena1 <desenabr@usc.edu>
2022-05-19 22:58:56 -0700

    adding auto detection of PMID

--- routes/geo.js
+++ routes/geo.js
@@ -66,6 +66,7 @@ router.get('/id/:sraId', async function(req, res, next) {
   var the_id = req.params.sraId;
   console.log('fetching data for ' + the_id);
 
+  /*****************  FETCH STUDY INFORMATION ***************/
   //get information from this SRX
   var ret;
   await axios.get(process.env.EFETCH_URL  + '&id=' + the_id).then((response) => {
@@ -74,23 +75,50 @@ router.get('/id/:sraId', async function(req, res, next) {
     });
   });
 
+  console.log(ret.STUDY[0].DESCRIPTOR[0].STUDY_TITLE[0]);
+
   // get study ID
   the_study = ret.EXPERIMENT[0].STUDY_REF[0].$.accession;
-  console.log('study: ' + the_study);
 
+  // get PMID (if it exists)
+  var PMID = 'NOTFOUND';
+  var the_gse = 'GSE-NOT-FOUND';
+  try {
+    const tmp = ret.STUDY[0].$.alias;
+    if (tmp.includes("GSE"))
+      the_gse = tmp;
+  }
+  catch (e) {
+    console.log('could not find GSE for this sample');
+  }
+  try {
+    const the_geo_url = process.env.GEO_URL+the_gse;
+    await axios.get(the_geo_url).then((response)  => {
+      PMID = response.data.match('\/pubmed\/[0-9]+')[0].replace('\/pubmed\/', '');
+    });
+  }
+  catch (e) {
+    console.log('could not find PMID for this study');
+    console.log(e);
+  }
+  console.log('study: ' + the_study);
+  console.log('GSE: ' + the_gse);
+  console.log('PMID: ' + PMID);
+
+  /*************** FETCH SAMPLE INFORMATION **************/
   // Get IDs of same study
   var same_study_ids;
   search_url = process.env.ESEARCH_URL_STUDY + '(' + the_study + '[Study] AND Bisulfite-Seq[Strategy])';
   await axios.get(search_url).then((response) => {
     xml2js.parseString(response.data, (err, result) => {
       same_study_ids = result.eSearchResult.IdList[0].Id;
-      console.log('hits: ' + same_study_ids.length);
       same_study_ids = same_study_ids.join(',');
     });
   });
 
   // get all SRR URLs from the same study
   var srr_urls = []
+  var srrs = []
   const fetch_url = process.env.EFETCH_URL  + '&id=' + same_study_ids;
   await axios.get(fetch_url).then((response) => {
     xml2js.parseString(response.data, (err, result) => {
@@ -105,6 +133,7 @@ router.get('/id/:sraId', async function(req, res, next) {
               if (typeof the_url !== "undefined") {
                 if (!found && good_url(the_url)) {
                   srr_urls.push(the_url);
+                  srrs.push(the_file.$.filename);
                   found = true;
                 }
               }
@@ -112,15 +141,17 @@ router.get('/id/:sraId', async function(req, res, next) {
               else if (typeof the_file.Alternatives !== "undefined") {
                 the_file.Alternatives.forEach((alternative) => {
                   const the_alt_url = alternative.$.url;
-                  if (!found && good_url(the_url)) {
+                  if (!found && good_url(the_alt_url)) {
                     srr_urls.push(the_alt_url);
+                    srrs.push(the_file.$.filename);
                     found = true;
                   }
                 });
               }
             }
             catch (e) {
-              console.log('Could not fetch data!');
+              console.log('Could not fetch URL!');
+              console.log('Error: ' + e);
             }
           });
         });
@@ -132,8 +163,16 @@ router.get('/id/:sraId', async function(req, res, next) {
     });
   });
 
-  console.log(srr_urls);
-  data = {title : 'GEO query ' + the_id, id : the_id, ret : ret, srr_urls : srr_urls};
+  console.log(srrs);
+  data = {title : 'GEO query ' + the_id,
+          id : the_id,
+          GSE : the_gse,
+          PMID : PMID,
+          project_name : 'FILLME',
+          organism: 'FINDTHEORGANISM',
+          ret : ret,
+          srrs : srrs,
+          srr_urls : srr_urls};
   res.render('geodataset', data);
 });
 
